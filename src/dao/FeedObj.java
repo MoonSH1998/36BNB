018f6be4745649ee89de8a014fc4e671ffa4bc5e
MoonSH1998 <tjdgk8880@naver.com>
2021-06-02 19:47:49 +0900

    main페이지 수정중

--- src/dao/FeedObj.java
+++ src/dao/FeedObj.java
@@ -1,11 +1,12 @@
 package dao;
 public class FeedObj {
-private String id, start, end, type, trader, content, ts, images;
-public FeedObj(String id, String type,  String start, String end,String trader, String content, String ts, String images) {
+private String id, start, end, type, trader, price, content, ts, images;
+public FeedObj(String id, String type,  String start, String end, String price,String trader, String content, String ts, String images) {
 this.id = id;
 this.start = start;
 this.end = end;
 this.type = type;
+this.price = price;
 this.trader = trader;
 this.content = content;
 this.ts = ts;
@@ -15,6 +16,7 @@ public String getId() { return this.id; }
 public String getStart() { return this.start; }
 public String getEnd() { return this.end; }
 public String getType() { return this.type; }
+public String getPrice() { return this.price; }
 public String getTrader() { return this.trader; }
 public String getContent() { return this.content; }
 public String getTs() { return this.ts; }
